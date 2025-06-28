664d8c019e7492fb1315a4df98b6973c74f52b2f
KumarTanmay554 <139523893+KumarTanmay554@users.noreply.github.com>
2025-06-28 20:51:44 +0530

    Recover in error

--- 13_Errors/main.go
+++ 13_Errors/main.go
@@ -20,9 +20,17 @@ func divideWithPanic(a, b int) int {
 }
 
 func main() {
+	defer func(){
+		if resp:= recover(); resp != nil{
+			fmt.Println("Recovered from panic:", resp)
+			fmt.Println("Helps server to continue running even after an error.")
+		}
+	}();
+
 	fmt.Println(divide(10,0))
 	fmt.Println("even if error is found, the program continues to run.")
 
 	fmt.Println(divideWithPanic(10,0))
 	fmt.Println("This line will not be executed due to the panic above.")
+
 }
