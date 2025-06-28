9a06ee043e705a44548c89ab811a75ab886f6add
KumarTanmay554 <139523893+KumarTanmay554@users.noreply.github.com>
2025-06-28 21:03:52 +0530

    recover from panic

--- 13_Errors/main.go
+++ 13_Errors/main.go
@@ -7,12 +7,19 @@ import (
 
 func divide(a, b int) (int,error) {
 	if b == 0 {
-		return 0, errors.New("division by zero is not allowed")
+		return 1, errors.New("division by zero is not allowed")
 	}
 	return a / b,nil
 }
 
-func divideWithPanic(a, b int) int {
+func divideWithPanic(a, b int) (res int) {
+	defer func(){
+		if resp:= recover(); resp != nil{
+			fmt.Println("Recovered from panic:", resp)
+			res = 3 //default value to return
+			// fmt.Println("Helps server to continue running even after an error.")
+		}
+	}();
 	if b == 0 {
 		panic("division by zero is not allowed")
 	}
@@ -20,17 +27,14 @@ func divideWithPanic(a, b int) int {
 }
 
 func main() {
-	defer func(){
-		if resp:= recover(); resp != nil{
-			fmt.Println("Recovered from panic:", resp)
-			fmt.Println("Helps server to continue running even after an error.")
-		}
-	}();
+	
 
 	fmt.Println(divide(10,0))
 	fmt.Println("even if error is found, the program continues to run.")
 
 	fmt.Println(divideWithPanic(10,0))
 	fmt.Println("This line will not be executed due to the panic above.")
+
+
 
 }
